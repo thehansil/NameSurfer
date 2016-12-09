ca317e1bf39a75ee983784e28e27ba68321b6b23
Jacob Hansil <jacobhansil@gmail.com>
2016-12-09 14:30:08 -0500

    Fixed Sanitization of data from database

--- NameSearch/name-helper.rb
+++ NameSearch/name-helper.rb
@@ -1,59 +1,82 @@
 require './babynames.rb'
+
 def create_name_array(babyname = 'A')
   name = NamesTable.first(:name=>babyname)
   if name
     array = []
-    if array.length == 0
-      pair = []
-      pair.push('1900')
-      pair.push(name.year1900.gsub(/[^0-9]+/,''))
-      array.push(pair)
-      pair = []
-      pair.push('1910')
-      pair.push(name.year1910.gsub(/[^0-9]+/,''))
-      array.push(pair)
-      pair = []
-      pair.push('1920')
-      pair.push(name.year1920.gsub(/[^0-9]+/,''))
-      array.push(pair)
-      pair = []
-      pair.push('1930')
-      pair.push(name.year1930.gsub(/[^0-9]+/,''))
-      array.push(pair)
-      pair = []
-      pair.push('1940')
-      pair.push(name.year1940.gsub(/[^0-9]+/,''))
-      array.push(pair)
-      pair = []
-      pair.push('1950')
-      pair.push(name.year1950.gsub(/[^0-9]+/,''))
-      array.push(pair)
-      pair = []
-      pair.push('1960')
-      pair.push(name.year1960.gsub(/[^0-9]+/,''))
-      array.push(pair)
-      pair = []
-      pair.push('1970')
-      pair.push(name.year1970.gsub(/[^0-9]+/,''))
-      array.push(pair)
-      pair = []
-      pair.push('1980')
-      pair.push(name.year1980.gsub(/[^0-9]+/,''))
-      array.push(pair)
-      pair = []
-      pair.push('1990')
-      pair.push(name.year1990.gsub(/[^0-9]+/,''))
-      array.push(pair)
-      pair = []
-      pair.push('2000')
-      pair.push(name.year2000.gsub(/[^0-9]+/,''))
-      array.push(pair)
+    pair = []
+    pair.push('1900')
+    if name.year1900.is_a? Integer
+      pair.push(name.year1900)
     end
+    array.push(pair)
+    pair = []
+    pair.push('1910')
+    if name.year1900.is_a? Integer
+      pair.push(name.year1910)
+    end
+    array.push(pair)
+    pair = []
+    pair.push('1920')
+    if name.year1900.is_a? Integer
+      pair.push(name.year1920)
+    end
+    array.push(pair)
+    pair = []
+    pair.push('1930')
+    if name.year1900.is_a? Integer
+      pair.push(name.year1930)
+    end
+    array.push(pair)
+    pair = []
+    pair.push('1940')
+    if name.year1900.is_a? Integer
+      pair.push(name.year1940)
+    end
+    array.push(pair)
+    pair = []
+    pair.push('1950')
+    if name.year1900.is_a? Integer
+      pair.push(name.year1950)
+    end
+    array.push(pair)
+    pair = []
+    pair.push('1960')
+    if name.year1900.is_a? Integer
+      pair.push(name.year1960)
+    end
+    array.push(pair)
+    pair = []
+    pair.push('1970')
+    if name.year1900.is_a? Integer
+      pair.push(name.year1970)
+    end
+    array.push(pair)
+    pair = []
+    pair.push('1980')
+    if name.year1900.is_a? Integer
+      pair.push(name.year1980)
+    end
+    array.push(pair)
+    pair = []
+    pair.push('1990')
+    if name.year1900.is_a? Integer
+      pair.push(name.year1990)
+    end
+    array.push(pair)
+    pair = []
+    pair.push('2000')
+    if name.year1900.is_a? Integer
+      pair.push(name.year2000)
+    end
+    array.push(pair)
+
     array.each do |v|
       if v[1] == 0
         v[1] = 1001
       end
     end
+
     array
   else
     nil
